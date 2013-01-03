7a635526f2ddfcb5ecc05ef5f3044a58b5825b1a
EvgenSk <evgensk@gmail.com>
2013-01-03 17:14:46 +0700

    Informer and updater are added

---
+++ informer.erl
@@ -0,0 +1,72 @@
+
+-module(informer).
+-behaviour(gen_server).
+
+-export([start_link/2, start_link/3]).
+-export([init/1, handle_call/3, handle_cast/2]).
+
+-include("definitions.hrl").
+
+%% 
+%% API (gen_server functions)
+%% 
+
+start_link(Time_interval, Update_now) ->
+    gen_server:start_link(?MODULE, [Time_interval, Update_now], []).
+
+start_link(ServerName, Time_interval, Update_now) ->
+    gen_server:start_link(ServerName, ?MODULE, [Time_interval, Update_now], []).
+
+init([Time_interval, Update_now]) ->
+    timer:send_interval(Time_interval, #data_req{request = update}), % may be we must use apply_interval instead of send_interval
+    case Update_now of
+	update -> 
+	    gen_server:cast(self(), #data_req{request = update}),
+	    {ok, #data_status{status = updating}};
+	_ -> 
+	    {ok, #data_status{status = up_to_date}}
+    end.
+
+
+%% handle_cast
+
+handle_cast(#data_req{request = update}, #data_status{status = up_to_date}) ->
+    gen_server:cast(?UPDATER, #data_req{request = update}),
+    {noreply, #data_status{status = updating}};
+
+handle_cast(#data_req{request = updating_finished}, _State) ->
+    {noreply, #data_status{status = up_to_date}}.
+
+handle_cast(_Msg, State)
+  when State = #data_status{status = updating} ->
+    {noreply, State}.
+
+%% handle_call
+    
+handle_call(#data_req{request = update}, _From, #data_status{status = ready}) ->
+    gen_server:cast(?UPDATER, #data_req{request = update}),
+    {noreply, #data_status{status = updating}};
+
+handle_call(#data_req{request = update}, _From, State) 
+  when State = #data_status{status = updating} ->
+    {noreply, State};
+
+handle_call(#data_req{request = info, info_type = updating_status}, _From, State) ->
+    {reply, State, State};
+
+handle_call(#data_req{request = info, info_type = zip_status, param = ZIP}, _From, State) ->
+    if
+	State == #data_status{status = updating} ; potraf:check_need_update(?RESULT, ZIP) -> 
+	    #data_status{status = updating};
+	_ -> #data_status{status = up_to_date}
+    end.
+
+%%  handle_info
+
+handle_info(#data_req{request = update}, #data_status{status = up_to_date}) ->
+    gen_server:cast(?UPDATER, #data_req{request = update}),
+    {noreply, #data_status{status = updating}};
+
+handle_info(_Msg, State)
+  when State = #data_status{status = updating} ->
+    {noreply, State}.
---
+++ updater.erl
@@ -0,0 +1,72 @@
+
+-module(updater).
+-behaviour(gen_server).
+
+-export([start_link/2, start_link/3]).
+-export([init/1, handle_call/3, handle_cast/2]).
+
+-include("definitions.hrl").
+
+%% 
+%% API (gen_server functions)
+%% 
+
+start_link(Update_now) ->
+    gen_server:start_link(?MODULE, [Update_now], []).
+
+start_link(ServerName, Update_now) ->
+    gen_server:start_link(ServerName, ?MODULE, [Update_now], []).
+
+init([Update_now]) ->
+    if Update_now == update -> 
+	    update_data()
+    end,
+    {ok, #data_status{status = up_to_date}}.
+
+
+%% handle_call
+
+handle_call(Req, _From, State) ->
+    gen_server:cast(self(), Req),
+    {reply, ok, State}.
+
+%% handle_cast
+
+handle_cast(#data_req{request = update}, _State) ->
+    update_data(),
+    {noreply, #data_status{status = up_to_date}};
+
+handle_cast(_Req, State) ->
+    {noreply, State}.
+
+%% handle_info
+
+handle_info(Info, State) ->
+    gen_server:cast(self(), Info),
+    {noreply, State}.
+
+%% 
+%% Internal functions
+%% 
+
+update_data() ->
+    Connection = potraf:get_connection(result),
+    update_each_zip(Connection),
+    %% TODO: also need to update timestamps
+    gen_server:cast(?INFORMER, #data_req{request = updating_finished}).
+
+update_each_zip(Connection) ->
+    ZIP = potraf:get_zip_for_upd(),
+    case ZIP of
+	undefined -> ok;
+	_ -> 
+	    Avg_vals = map(fun(Param) -> 
+				   {Param, 
+				    potraf:get_average_for_time(Connection, {minute, 5}, ZIP, Param)} end, % {minute, 5} must be somethiing else for increment
+			   record_info(fields, traffic)),
+	    foreach(fun({Id, Val}) -> potraf:set(Connection, ZIP, Id, Val) end, 
+		    Avg_vals),
+	    %% TODO: also need to update timestamps
+	    update_each_zip(Connection)
+    end.
+    

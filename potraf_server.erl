a49655e5f5366928d64e2b2654975275529672eb
EvgenSk <evgensk@gmail.com>
2013-01-03 17:13:28 +0700

    Implementation of internal functions

--- potraf_server.erl
+++ potraf_server.erl
@@ -37,15 +37,14 @@ handle_call(#potraf_req{type = update}, _From, State) ->
 	 _ -> {noreply, State}
     end;
 
-handle_call(#potraf_req{type = get, param = ZIP}, _From, State) 
-  when State = #potraf_state{readiness = ready} or
-       waiting_for_update(ZIP) = false ->
+handle_call(#potraf_req{type = get, param = ZIP}, _From, State) ->
+    wait_data_ready(ZIP),
     Traffic = get_traffic_info(ZIP),
     Timestamps = get_timestamps(ZIP),
-    {reply, {Traffic, Timestamps}, State}. % TODO: may be we need record for reply {Traffic, Timestamps}
+    {reply, {Traffic, Timestamps}, State}; % TODO: may be we need record for reply {Traffic, Timestamps}
 
-handle_call(#potraf_req{type = get, param = ZIP}, From, State) ->
-    %% TODO: wait for updating finish, use 'From' for it
+handle_call(#potraf_req{type = add, param = {ZIP, Traf_info}}, _From, State) ->
+    add_traffic_and_timestamps_info(ZIP, Traf_info),
     {noreply, State}.
 
 %% handle_cast
@@ -53,17 +52,27 @@ handle_call(#potraf_req{type = get, param = ZIP}, From, State) ->
 handle_cast(#potraf_req{type = update}, State) 
   when State = #potraf_state{readiness = ready}->
     gen_server:cast(?UPDATER, #potraf_req{type = update}), % TODO: may be better would be to rise new process for updating every time
-    {noreply, #potraf_state{readiness = updating}}.
+    {noreply, #potraf_state{readiness = updating}};
 
 handle_cast(#potraf_req{type = update}, State) 
   when State = #potraf_state{readiness = updating} ->
-    {noreply, State}.
+    {noreply, State};
+
+handle_cast(#potraf_req{type = add, param = {ZIP, Traf_info}}, State) ->
+    add_traffic_info(ZIP, Traf_info),
+    {noreply, State};
 
 handle_cast(updated, _State) ->			% TODO: move 'updated' to some record?
-    {noreply, #potraf_state{readiness = ready}}.
+    {noreply, #potraf_state{readiness = ready}};
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
 
 
 %% handle_info
+
+handle_info(_Message, State) ->
+    {noreply, State}.
 
 
 %% 
@@ -83,3 +92,45 @@ get_timestamps(ZIP) ->
 		increment = potraf:get_params_timestamp(?RESULT, ZIP, increment),
 		post_windows_count = potraf:get_params_timestamp(?RESULT, ZIP, post_windows_count),
 		package_windows_count = potraf:get_params_timestamp(?RESULT, ZIP, package_windows_count)}.
+
+add_traffic_info(ZIP, Traf_info) ->
+    foreach(fun({Id, Val}) -> potraf:add(?RAW_DATA, ZIP, Id, Val) end,
+	    record_to_tuplelist(traffic, Traf_info)).
+
+add_timestamps_info(ZIP, Timestamp) ->
+    Fields = record_info(fields, traffic),
+    foreach(fun(Field) -> potraf:add_params_timestamp(?RAW_DATA, ZIP, Field, Timestamp) end, 
+	    Fields).
+
+set_last_timestamps(ZIP, Traf_info, Timestamp) ->
+    foreach(fun({Id, Val}) -> potraf:set_last_timestamps(?RESULT, ZIP, Id, Timestamp) end,
+	    filter(fun({_, Val})-> potraf:is_useful(Val) end, 
+		   record_to_tuplelist(traffic, Traf_info))).
+
+add_traffic_and_timestamps_info(ZIP, Traf_info) ->
+    add_traffic_info(ZIP, Traf_info),
+    Timestamp = now(),
+    add_timestamps_info(ZIP, Timestamp),
+    set_last_timestamps(ZIP, Traf_info, Timestamp).
+
+get_data_status(ZIP) ->
+    case get_updating_status() of
+	ready -> ready;
+	_ -> check_need_update(ZIP)
+    end.
+
+get_updating_status() ->
+    {reply, Reply} = gen_server:call(?UPDATER, #data_req{request = info, info_type = updating_status}),
+    Reply.
+
+check_need_update(ZIP) ->
+    {reply, Reply} = gen_server:call(?UPDATER, #data_req{request = info, info_type = zip_status, param = ZIP}),
+    Reply.
+	    
+wait_data_ready(ZIP) ->
+    case get_data_status(ZIP) of
+	ready -> ready;
+	_ -> wait_data_ready(ZIP)
+    end.
+	  
+    

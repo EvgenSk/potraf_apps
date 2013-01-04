fe1318798abe5ae9d85d776ac2645833acd0cb2e
EvgenSk <evgensk@gmail.com>
2013-01-05 01:01:24 +0700

    get_potraf_req and send_potaf_req implemented

--- http_gate/src/potraf_http_handler.erl
+++ http_gate/src/potraf_http_handler.erl
@@ -19,7 +19,8 @@ handle(Req, State) ->
     {Method, Req2} = cowboy_req:method(Req),
     Potraf_req = get_potraf_req(Method, Req2),
     Potraf_repl = send_potraf_req(Potraf_req),
-    {ok, Potraf_repl, State}.
+    Repl = wrap_potraf_repl(Potraf_repl),
+    {ok, Repl, State}.
 
 terminate(_Req, _State) ->
     ok.
@@ -34,19 +35,34 @@ get_potraf_req(<<"GET">>, Req) ->
 				    binary_to_list(Val)}
 		 end, 
 		 QSVals_bin),
-    case proplists:get_value(type) of
-	get -> get_info(ZIP);
-	add -> add_info(ZIP, QSVals)	    
+    case proplists:get_value(request) of
+	get -> get_req(ZIP);
+	add -> add_req(ZIP, QSVals)	    
     end.
     
-get_info(ZIP) ->
-    %% create potraf_req,
-    %% create potraf_client,
-    %% request info from it
-    ok.
+get_req(ZIP) ->
+    #potraf_req{request = get,
+		param = ZIP}.
 
-add_info(ZIP, QSVals) ->
-    %% create potraf_req,
-    %% create potraf_client,
-    %% request add info
+add_req(ZIP, QSVals) ->
+    #potraf_req{request = add,
+		param = 
+		    {ZIP,
+		    #traffic{
+		       people_count = proplists:get_value(people_count),
+		       service_time = proplists:get_value(service_time),
+		       post_windows_count = proplists:get_value(post_windows_count),
+		       package_windows_count = proplists:get_value(package_windows_count)}}}.
+
+send_potraf_req(Req)
+  when Req = #potraf_req{request = get, param = ZIP} ->
+    {ok, Client} = potraf_client:start_link(),
+    %% may ne add Client to potraf_sup
+    gen_server:call(Client, Req);
+
+send_potraf_req(Req) 
+  when #potraf_req{request = add} = Req ->
+    {ok, Client} = potraf:start_link(),
+    %% may ne add Client to potraf_sup
+    gen_server:cast(Client, Req),
     ok.

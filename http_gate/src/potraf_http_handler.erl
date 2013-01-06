2f6ff157295ab89cc76522152523d433d3186872
EvgenSk <evgensk@gmail.com>
2013-01-06 19:01:00 +0700

    Compilation errors fixed

--- http_gate/src/potraf_http_handler.erl
+++ http_gate/src/potraf_http_handler.erl
@@ -6,7 +6,9 @@
 -export([handle/2]).
 -export([terminate/2]).
 
--include_lib("potraf/include/definitions.hrl").
+-include_lib("../../potraf/include/definitions.hrl").
+
+-import(lists, [map/2]).
 
 %% 
 %% API
@@ -17,10 +19,10 @@ init(_Transport, Req, [Repl_type]) ->
 
 handle(Req, Repl_type) ->
     {Method, Req2} = cowboy_req:method(Req),
-    Potraf_req = get_potraf_req(Method, Req2),
+    {Potraf_req, Req3} = get_potraf_req(Method, Req2),
     Potraf_repl = send_potraf_req(Potraf_req),
-    {ok, Req3} = send_response(Repl_type, Potraf_repl, Req2),
-    {ok, Req3, State}.
+    {ok, Req4} = send_response(Repl_type, Potraf_repl, Req3),
+    {ok, Req4, Repl_type}.
 
 terminate(_Req, _State) ->
     ok.
@@ -30,38 +32,36 @@ terminate(_Req, _State) ->
 %% 
 
 get_potraf_req(<<"GET">>, Req) ->
-    {QSVals_bin, Req2} = cowboy_req:qs_vals(),
-    QSVals = map(fun({Key, Val})-> {binary_to_atom(Key), 
+    {QSVals_bin, Req2} = cowboy_req:qs_vals(Req),
+    QSVals = map(fun({Key, Val})-> {binary_to_atom(Key, latin1), 
 				    binary_to_list(Val)}
 		 end, 
 		 QSVals_bin),
     case proplists:get_value(request) of
-	get -> get_req(ZIP);
-	add -> add_req(ZIP, QSVals)	    
+	get -> {get_req(proplists:get_value(zip, QSVals)), Req2};
+	add -> {add_req(QSVals), Req2}	        
     end.
     
 get_req(ZIP) ->
     #potraf_req{request = get,
 		param = ZIP}.
 
-add_req(ZIP, QSVals) ->
+add_req(QSVals) ->
     #potraf_req{request = add,
 		param = 
-		    {ZIP,
+		    {proplists:get_value(zip, QSVals),
 		    #traffic{
-		       people_count = proplists:get_value(people_count),
-		       service_time = proplists:get_value(service_time),
-		       post_windows_count = proplists:get_value(post_windows_count),
-		       package_windows_count = proplists:get_value(package_windows_count)}}}.
+		       people_count = proplists:get_value(people_count, QSVals),
+		       service_time = proplists:get_value(service_time, QSVals),
+		       post_windows_count = proplists:get_value(post_windows_count, QSVals),
+		       package_windows_count = proplists:get_value(package_windows_count, QSVals)}}}.
 
-send_potraf_req(Req)
-  when Req = #potraf_req{request = get, param = ZIP} ->
+send_potraf_req(Req = #potraf_req{request = get}) ->
     {ok, Client} = potraf_client:start_link(),
     %% may ne add Client to potraf_sup
     gen_server:call(Client, Req);
 
-send_potraf_req(Req) 
-  when #potraf_req{request = add} = Req ->
+send_potraf_req(Req = #potraf_req{request = add}) ->
     {ok, Client} = potraf:start_link(),
     %% may ne add Client to potraf_sup
     gen_server:cast(Client, Req),
@@ -70,10 +70,10 @@ send_potraf_req(Req)
 send_potraf_req(_Req) ->
     undefined.
 
-send_response(undefined, Req) ->
-    cowboy_req:reply(400, [], <<"Missing request parameters.">>);
+send_response(_Repl_type, undefined, Req) ->
+    cowboy_req:reply(400, [], <<"Missing request parameters.">>, Req);
 
-send_response(ok, Req) ->
+send_response(_Repl_type, ok, Req) ->
     cowboy_req:reply(200, [{<<"content-encoding">>, <<"utf-8">>}], "ok", Req);
 
 send_response(Reply_type, Reply, Req) ->
@@ -85,7 +85,7 @@ send_response(Reply_type, Reply, Req) ->
 
 wrap_potraf_repl(simple, {Traf, Time}) ->
     string:join([to_KeyVal_list_string(Traf), 
-		 to_KeyVal_list_string(Traf)], 
+		 to_KeyVal_list_string(Time)], 
 		"|").
 
 to_KeyVal_list_string(Rec) ->
@@ -93,5 +93,5 @@ to_KeyVal_list_string(Rec) ->
 	map(fun({Id, Val}) -> 
 		    string:join([atom_to_list(Id), float_to_list(Val)], ":") 
 	    end,
-	    record_to_tuplelist(traffic, Rec)),
+	    ?record_to_tuplelist(traffic, Rec)),
     string:join(KeyVal_pairs, ";").

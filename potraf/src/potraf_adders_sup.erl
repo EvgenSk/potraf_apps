64c1761ea9c485b813e4aa81ecaa9ab2b6c3c566
EvgenSk <evgensk@gmail.com>
2013-01-10 00:07:30 +0700

    Added supervisor for potraf_adders

---
+++ potraf/src/potraf_adders_sup.erl
@@ -0,0 +1,19 @@
+
+-module(potraf_adders_sup).
+-behaviour(supervisor).
+
+-export([start_link/0]).
+-export([init/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+    {ok, {{simple_one_for_one, 5, 60},
+	  [potraf_adder,
+	   {potraf_adder, start_link, []},
+	   transient, 
+	   infinity, 
+	   worker, 
+	   [potraf_adder]
+	  ]}}.

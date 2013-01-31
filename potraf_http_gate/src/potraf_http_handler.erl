916160f9ec523207cfac6b08e6323bc073aede7d
EvgenSk <evgensk@gmail.com>
2013-01-31 23:08:03 +0700

    Using new potraf-requests API; POST-HTTP are used only for add-potraf requests and GET-HTTP requests - for get-potraf requests

--- potraf_http_gate/src/potraf_http_handler.erl
+++ potraf_http_gate/src/potraf_http_handler.erl
@@ -6,8 +6,6 @@
 -export([handle/2]).
 -export([terminate/2]).
 
--include_lib("definitions.hrl").
-
 -import(lists, [map/2, zip/2]).
 
 %% 
@@ -19,9 +17,9 @@ init(_Transport, Req, [Repl_type]) ->
 
 handle(Req, Repl_type) ->
     {Method, Req2} = cowboy_req:method(Req),
-    {Potraf_req, Req3} = get_potraf_req(Method, Req2),
+    Potraf_req = get_potraf_req(Method, Req2),
     Potraf_repl = send_potraf_req(Potraf_req),
-    {ok, Req4} = send_response(Repl_type, Potraf_repl, Req3),
+    {ok, Req4} = send_response(Repl_type, Potraf_repl, Req2),
     {ok, Req4, Repl_type}.
 
 terminate(_Req, _State) ->
@@ -32,65 +30,42 @@ terminate(_Req, _State) ->
 %% 
 
 get_potraf_req(<<"GET">>, Req) ->
-    case cowboy_req:qs_val(<<"request">>, Req) of
-    	{<<"get">>, Req2} -> {get_req(get, Req2), Req2};
-    	{<<"add">>, Req2} -> {add_req(get, Req2), Req2}
-    end;
+    {get, get_req(Req)};
 
 get_potraf_req(<<"POST">>, Req) ->
     case cowboy_req:has_body(Req) of
-	{true, Req2} -> {fetch_req(Req2), Req2};
-	{false, Req2} -> {ok, Req2}
+	{true, _Req2} -> {add, add_req(Req)};
+	{false, _Req2} -> ok
     end.
     
 %% for GET HTTP-requests
 
-get_req(get, Req) ->
+get_req(Req) ->
+    fetch_zip(Req).
+
+add_req(Req) ->
+    ZIP = fetch_zip(Req),
+    {ok, PostVals, _} = cowboy_req:body_qs(Req),    
+    KeyVals = map(fun({Key, Val})-> {binary_to_atom(Key, latin1), qs_val_to_int(Val)}end,
+		  map(fun(Key)-> {Key, proplists:get_value(Key, PostVals)} end,
+		      proplists:get_keys(PostVals))),
+    {ZIP, KeyVals}.
+
+fetch_zip(Req) ->
     {ZIP_bin, _Req2} = cowboy_req:qs_val(<<"zip">>, Req),
-    ZIP = list_to_integer(binary_to_list(ZIP_bin)),
-    #potraf_req{request = get,
-		param = ZIP};
+    list_to_integer(binary_to_list(ZIP_bin)).
 
-get_req(post, PostVals) ->
-    ZIP_bin = proplists:get_value(<<"zip">>, PostVals),
-    ZIP = list_to_integer(binary_to_list(ZIP_bin)),
-    #potraf_req{request = get,
-		param = ZIP}.
+send_potraf_req(ok)->
+    ok;
 
-add_req(get, Req) ->
-    get_potraf_req_by_func(fun(Id) -> {Val, _} = cowboy_req:qs_val(atom_to_binary(Id, latin1), Req), 
-				      Val end);
+send_potraf_req(undefined)->
+    undefined;
 
-add_req(post, PostVals) ->
-    get_potraf_req_by_func(fun(Id) -> proplists:get_value(atom_to_binary(Id, latin1), PostVals) end).
+send_potraf_req({get, ZIP})->
+    potraf:request(get, ZIP);
 
-%% for POST HTTP-requests
-
-fetch_req(Req) ->
-    {ok, PostVals, _} = cowboy_req:body_qs(Req),
-    case proplists:get_value(<<"request">>, PostVals) of
-	<<"get">> -> get_req(post, PostVals);
-	<<"add">> -> add_req(post, PostVals)
-    end.
-    
-get_potraf_req_by_func(Func) ->
-    Vals = map(fun(Val) -> qs_val_to_int(Val) end,
-	       map(fun(Id)-> Func(Id) end,
-		   record_info(fields, traffic))),
-    KeyVals = lists:zip(record_info(fields, traffic), Vals),
-    ZIP_bin = Func(zip),
-    ZIP = list_to_integer(binary_to_list(ZIP_bin)),
-    #potraf_req{request = add,
-		param = 
-		    {ZIP,
-		    #traffic{
-		       people_count = proplists:get_value(people_count, KeyVals),
-		       service_time = proplists:get_value(service_time, KeyVals),
-		       post_windows_count = proplists:get_value(post_windows_count, KeyVals),
-		       package_windows_count = proplists:get_value(package_windows_count, KeyVals)}}}.    
-
-send_potraf_req(Req) ->
-    potraf:request(Req).
+send_potraf_req({add, Params})->
+    potraf:request(add, Params).
 
 send_response(_Repl_type, undefined, Req) ->
     cowboy_req:reply(400, [], <<"Missing request parameters.">>, Req);

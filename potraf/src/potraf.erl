93b9a11d4e78c86263c1fc246c21cd8f6015b57c
EvgenSk <evgensk@gmail.com>
2013-01-31 23:05:34 +0700

    Requests API changed

--- potraf/src/potraf.erl
+++ potraf/src/potraf.erl
@@ -3,22 +3,24 @@
 
 -module(potraf).
 
--export([request/1]).
+-export([request/2]).
 
 -include_lib("definitions.hrl").
 
 -define(name(ZIP), list_to_atom(lists:concat(["adder", utils:to_list(ZIP)]))).
 
-request(Req = #potraf_req{request = get}) ->
-    get_data(Req#potraf_req.param);
+%% API
 
-request(Req = #potraf_req{request = add}) ->
-    {ZIP, Traffic} = Req#potraf_req.param,
-    add_data(ZIP, Traffic);
+request(get, ZIP)->
+    get_data(ZIP);
 
-request(_Req) ->
+request(add, {ZIP, KeyVals}) ->
+    add_data(ZIP, keyvals_to_traffic(KeyVals));
+
+request(_, _Req) ->
     undefined.
 
+%% Internal functions
 
 get_data(ZIP) ->
     {ok, C} = supervisor:start_child(potraf_getters_sup, []),
@@ -35,3 +37,11 @@ zip_traf_info(Traf_info, Timestamps) ->
     lists:zip3(record_info(fields, traffic), 
 	       tl(tuple_to_list(Traf_info)),
 	       tl(tuple_to_list(Timestamps))).
+
+keyvals_to_traffic(KeyVals) ->
+    #traffic{
+	      people_count = proplists:get_value(people_count, KeyVals),
+	      service_time = proplists:get_value(service_time, KeyVals),
+	      post_windows_count = proplists:get_value(post_windows_count, KeyVals),
+	      package_windows_count = proplists:get_value(package_windows_count, KeyVals)
+	    }.
